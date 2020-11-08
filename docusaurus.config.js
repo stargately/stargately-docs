f09ca5e8a849dc5993f919a93e591050bb0383af
Tian Pan <puncsky@gmail.com>
2020-11-07 22:15:17 -0800

    update edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,13 +86,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/puncsky/stargately-docs/edit/master/website/',
+            'https://github.com/puncsky/stargately-docs/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/puncsky/stargately-docs/edit/master/website/blog/',
+            'https://github.com/puncsky/stargately-docs/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
