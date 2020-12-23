8f388c67bdad447d5bc096ac288a23b73a932d8a
Tian Pan <puncsky@gmail.com>
2020-12-23 02:40:51 -0800

    update tagline

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,7 +63,7 @@ function Home() {
     >
       <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
+          <h1 className="hero__title">{Math.random() > 0.5 ? siteConfig.title : "Think big and act on it"}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
